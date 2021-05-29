08117139f3979ac9e425e203eb58d312713aa039
GabrielDCastro <gabrielrangel12@gmail.com>
2021-05-29 12:06:11 -0300

    adicionando parallax

--- main.py
+++ main.py
@@ -2,36 +2,42 @@ import pygame, sys
 clock = pygame.time.Clock()
 from pygame.locals import *
 
-pygame.init()
-pygame.display.set_caption('Frost Soul')
-WINDOW_SIZE = (1000,600)
-screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32) #inicia a janela
-display = pygame.Surface((300,200))
+pygame.init()  # initiates pygame
+pygame.display.set_caption('Pygame Platformer')
+WINDOW_SIZE = (600, 400)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # initiate the window
+display = pygame.Surface((300, 200))  # used as the surface for rendering, which is scaled
 
 moving_right = False
 moving_left = False
 vertical_momentum = 0
 air_timer = 0
-game_map = [['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','0','0','0','0','2','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','2','2','2','2','1','0','0','0','0','0','0','0'],
-            ['0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'],
-            ['2','2','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','2','2'],
-            ['1','1','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1','1'],
-            ['1','1','2','2','2','2','2','2','2','2','2','2','2','2','2','2','2','1','1'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'],
-            ['1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1','1']]
+
+true_scroll = [0, 0]
+
+def load_map(path):
+    f = open(path + '.txt', 'r')
+    data = f.read()
+    f.close()
+    data = data.split('\n')
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+
+
+game_map = load_map('map')
 
 grass_img = pygame.image.load('grass.png')
 dirt_img = pygame.image.load('dirt.png')
+
 player_img = pygame.image.load('hero2.png').convert()
-player_img.set_colorkey((255,255,255)) #se tiver alguma parte branca na imagem ele vai remover
-player_rect = pygame.Rect(100,100,39,25) #Localização do poersonagem e colizão. os dois ultimos têm que ser do tamanho da imagem
-#O a imagem do player é 39px por 25px
+player_img.set_colorkey((255, 255, 255))
+
+player_rect = pygame.Rect(100, 100, 39, 25)
+
+background_objects = [[0.25, [120, 10, 70, 400]], [0.25, [280, 30, 40, 400]], [0.5, [30, 40, 40, 400]],
+                      [0.5, [130, 90, 100, 400]], [0.5, [300, 80, 120, 400]]]
 
 
 def collision_test(rect, tiles):
@@ -68,15 +74,31 @@ def move(rect, movement, tiles):
 while True:  # game loop
     display.fill((146, 244, 255))  # clear screen by filling it with blue
 
+    true_scroll[0] += (player_rect.x - true_scroll[0] - 169) / 20
+    true_scroll[1] += (player_rect.y - true_scroll[1] - 112) / 20
+    scroll = true_scroll.copy()
+    scroll[0] = int(scroll[0])
+    scroll[1] = int(scroll[1])
+
+    pygame.draw.rect(display, (7, 80, 75), pygame.Rect(0, 120, 300, 80))
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1][0] - scroll[0] * background_object[0],
+                               background_object[1][1] - scroll[1] * background_object[0], background_object[1][2],
+                               background_object[1][3])
+        if background_object[0] == 0.5:
+            pygame.draw.rect(display, (14, 222, 150), obj_rect)
+        else:
+            pygame.draw.rect(display, (9, 91, 85), obj_rect)
+
     tile_rects = []
     y = 0
     for layer in game_map:
         x = 0
         for tile in layer:
             if tile == '1':
-                display.blit(dirt_img, (x * 16, y * 16))
+                display.blit(dirt_img, (x * 16 - scroll[0], y * 16 - scroll[1]))
             if tile == '2':
-                display.blit(grass_img, (x * 16, y * 16))
+                display.blit(grass_img, (x * 16 - scroll[0], y * 16 - scroll[1]))
             if tile != '0':
                 tile_rects.append(pygame.Rect(x * 16, y * 16, 16, 16))
             x += 1
@@ -100,7 +122,7 @@ while True:  # game loop
     else:
         air_timer += 1
 
-    display.blit(player_img, (player_rect.x, player_rect.y))
+    display.blit(player_img, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
     for event in pygame.event.get():  # event loop
         if event.type == QUIT:
