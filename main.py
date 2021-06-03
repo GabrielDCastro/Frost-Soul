914404a21d3c6a594b732d9eafe4f84dfef73fe8
GabrielDCastro <gabrielrangel12@gmail.com>
2021-06-02 21:00:02 -0300

    começando animações

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import pygame, sys
 clock = pygame.time.Clock()
 from pygame.locals import *
-
 pygame.init()  # initiates pygame
 pygame.display.set_caption('Pygame Platformer')
-WINDOW_SIZE = (600, 400)
+WINDOW_SIZE = (900, 600)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)  # initiate the window
-display = pygame.Surface((300, 200))  # used as the surface for rendering, which is scaled
+display = pygame.Surface((600, 300))  # used as the surface for rendering, which is scaled
 
 moving_right = False
 moving_left = False
@@ -25,16 +24,45 @@ def load_map(path):
         game_map.append(list(row))
     return game_map
 
+global animation_frames
+animation_frames = {}
+
+def load_animation(path, frame_durations):
+    global animation_frames
+    animation_name = path.split('/')[-1]
+    animation_frame_data = []
+    n = 0
+    for frame in frame_durations:
+        animation_frame_id = animation_name + '_' + str(n)
+        img_location = path + '/' + animation_frame_id + '.png'
+        animation_image = pygame.image.load(img_location).convert()
+        animation_image.set_colorkey((0,0,0)) #o fundo do personagem está preto, isso tira a cor preta de fundo
+        animation_frames[animation_frame_id] = animation_image.copy()
+        for i in range(frame):
+            animation_frame_data.append(animation_frame_id)
+        n += 1
+    return animation_frame_data
+
+def change_action(action_var, frame, new_value):
+    if action_var != new_value:
+        action_var = new_value
+        frame = 0
+    return action_var, frame
+
+animation_database = {}
+animation_database['correr'] = load_animation('player_animations/correr', [7,7,7])
+animation_database['parado'] = load_animation('player_animations/parado', [7,7,7])
+
+player_action = 'parado'
+player_frame = 0
+player_flip = False
 
 game_map = load_map('map')
 
 grass_img = pygame.image.load('chao_neve.png')
 dirt_img = pygame.image.load('chao_terra.png')
 
-player_img = pygame.image.load('hero2.png').convert()
-player_img.set_colorkey((255, 255, 255))
-
-player_rect = pygame.Rect(100, 100, 39, 25)
+player_rect = pygame.Rect(100, 100, 39, 45)
 
 background_objects = [[0.25, [120, 10, 70, 400]], [0.25, [280, 30, 40, 400]], [0.5, [30, 40, 40, 400]],
                       [0.5, [130, 90, 100, 400]], [0.5, [300, 80, 120, 400]]]
@@ -74,8 +102,8 @@ def move(rect, movement, tiles):
 while True:  # game loop
     display.fill((194, 226, 255))  # coloca cor de fundo de azul quase branco
 
-    true_scroll[0] += (player_rect.x - true_scroll[0] - 169) / 20
-    true_scroll[1] += (player_rect.y - true_scroll[1] - 112) / 20
+    true_scroll[0] += (player_rect.x - true_scroll[0] - 169) / 20 #posição da camera x
+    true_scroll[1] += (player_rect.y - true_scroll[1] - 112) / 20 #posição da camera y
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
     scroll[1] = int(scroll[1])
@@ -85,7 +113,7 @@ while True:  # game loop
         obj_rect = pygame.Rect(background_object[1][0] - scroll[0] * background_object[0],
                                background_object[1][1] - scroll[1] * background_object[0], background_object[1][2],
                                background_object[1][3])
-        if background_object[0] == 0.5:
+        if background_object[0] == 1:
             pygame.draw.rect(display, (61, 201, 255), obj_rect)
         else:
             pygame.draw.rect(display, (234, 255, 255), obj_rect)
@@ -96,24 +124,32 @@ while True:  # game loop
         x = 0
         for tile in layer:
             if tile == '1':
-                display.blit(dirt_img, (x * 16 - scroll[0], y * 16 - scroll[1]))
+                display.blit(dirt_img, (x * 32 - scroll[0], y * 32 - scroll[1]))
             if tile == '2':
-                display.blit(grass_img, (x * 16 - scroll[0], y * 16 - scroll[1]))
+                display.blit(grass_img, (x * 32 - scroll[0], y * 32 - scroll[1]))
             if tile != '0':
-                tile_rects.append(pygame.Rect(x * 16, y * 16, 16, 16))
+                tile_rects.append(pygame.Rect(x * 32, y * 32, 32, 32))
             x += 1
         y += 1
 
     player_movement = [0, 0]
     if moving_right == True:
-        player_movement[0] += 2
+        player_movement[0] += 4
     if moving_left == True:
-        player_movement[0] -= 2
+        player_movement[0] -= 4
     player_movement[1] += vertical_momentum
     vertical_momentum += 0.2
     if vertical_momentum > 3:
         vertical_momentum = 3
 
+    if player_movement[0] > 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'correr')
+        player_flip = False
+    if player_movement[0] == 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'parado')
+    if player_movement[0] < 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'correr')
+        player_flip = True
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
 
     if collisions['bottom'] == True:
@@ -122,7 +158,12 @@ while True:  # game loop
     else:
         air_timer += 1
 
-    display.blit(player_img, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+    player_frame += 1
+    if player_frame >= len(animation_database[player_action]):
+        player_frame = 0
+    player_img_id = animation_database[player_action][player_frame]
+    player_img = animation_frames[player_img_id]
+    display.blit(pygame.transform.flip(player_img,player_flip, False,),(player_rect.x - scroll[0], player_rect.y - scroll[1]))
 
     for event in pygame.event.get():  # event loop
         if event.type == QUIT:
@@ -134,8 +175,8 @@ while True:  # game loop
             if event.key == K_LEFT:
                 moving_left = True
             if event.key == K_UP:
-                if air_timer < 6:
-                    vertical_momentum = -5
+                if air_timer < 7:
+                    vertical_momentum = -6
         if event.type == KEYUP:
             if event.key == K_RIGHT:
                 moving_right = False
